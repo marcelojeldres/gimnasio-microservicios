6339a7eff98bd82445f0025f4d2ce341ec6b0fa7
BenjaminDuran <benj.durans@duocuc.cl>
2026-06-15 00:47:07 -0400

    Update Resena.java
    se ajusta texto de bean validation

--- ms-resenas/src/main/java/com/gimnasio/resenas/model/Resena.java
+++ ms-resenas/src/main/java/com/gimnasio/resenas/model/Resena.java
@@ -19,17 +19,17 @@ public class Resena {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "El ID del equipo es obligatorio")
+    @NotNull(message = "El ID del equipo es obligatorio!")
     private Long equipoId;
 
-    @NotBlank(message = "El RUN del cliente es obligatorio")
+    @NotBlank(message = "El RUN del cliente es obligatorio!")
     private String runCliente;
 
-    @NotNull(message = "La calificación es obligatoria")
+    @NotNull(message = "La calificación es obligatoria!")
     @Min(1) @Max(5)
     private Integer estrellas;
 
-    @NotBlank(message = "El comentario no puede estar vacío")
+    @NotBlank(message = "El comentario no puede estar vacío!")
     @Size(max = 500)
     private String comentario;
 
